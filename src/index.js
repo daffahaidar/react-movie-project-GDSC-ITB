5e6df300669646c5be2f8696d0aec0514136369d
daffahaidar <daffa.h.n.zufar@gmail.com>
2021-11-13 09:15:01 +0700

    Version 1.0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { CookiesProvider } from "react-cookie";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
